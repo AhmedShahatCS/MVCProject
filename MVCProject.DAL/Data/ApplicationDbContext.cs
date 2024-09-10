323e925e8bce703b53bee5c43995abdc6d8d7f45
AhmedShahat012 <ggndgm@gmail.com>
2024-09-10 19:03:27 +0300

    DAL for Department Module

---
+++ MVCProject.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProject.DAL.Data.Configuration;
+using MVCProject.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProject.DAL.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Database=MVCApplication; Trusted_Connection=true; TrustServerCertificate=true");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
