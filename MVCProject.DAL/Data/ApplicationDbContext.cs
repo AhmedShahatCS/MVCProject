089eb9b60ca740b1cfd6acf302976abbdf229434
AhmedShahat012 <ggndgm@gmail.com>
2024-09-11 02:53:29 +0300

    DbContext-Dependancy Injection And AppSetting File

--- MVCProject.DAL/Data/ApplicationDbContext.cs
+++ MVCProject.DAL/Data/ApplicationDbContext.cs
@@ -12,15 +12,19 @@ namespace MVCProject.DAL.Data
 {
     public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext( DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=.; Database=MVCApplication; Trusted_Connection=true; TrustServerCertificate=true");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=.; Database=MVCApplication; Trusted_Connection=true; TrustServerCertificate=true");
+        //}
 
         public DbSet<Department> Departments { get; set; }
     }
