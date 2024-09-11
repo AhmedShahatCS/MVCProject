0d91eb3e6d1f452f6a276a6fa7076e7154ef866f
AhmedShahat012 <ggndgm@gmail.com>
2024-09-11 20:49:10 +0300

    Create Actions Of Controller And Views of it

--- MVCProject.DAL/Entities/Department.cs
+++ MVCProject.DAL/Entities/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,12 @@ namespace MVCProject.DAL.Entities
 
 
         public int Id { get; set; }
+        [Required(ErrorMessage ="code is required")]
         public string Code { get; set; }
+        [Required(ErrorMessage = "Name is required")]
 
         public string Name { get; set; }
+        [Display(Name = "Date Of Creation")]
 
         public DateTime DateOfCreation { get; set; }
 
---
+++ MVCProject.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.BLL.Interfaces;
+using MVCProject.DAL.Entities;
+
+namespace MVCProject.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository departRepo;
+
+        public DepartmentController(IDepartmentRepository  DepartRepo)
+        {
+            departRepo = DepartRepo;
+        }
+        public IActionResult Index()
+        {
+            var res = departRepo.GetAll();
+            return View(res);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department dept)
+        {
+            if (ModelState.IsValid)
+            {
+                var count = departRepo.Add(dept);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(dept);
+        }
+
+        public IActionResult Details(int id)
+        {
+            if (id == null) return BadRequest();
+            var res = departRepo.Get(id);
+            if (res == null) return NotFound();
+            return View(res);
+
+        }
+    }
+}
